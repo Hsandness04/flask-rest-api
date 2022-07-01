1ecf8472156b5cb394ee0ac7881c4f7d8fdaae76
Hunter Sandness <huntersandness@Hunters-MacBook-Air-3.local>
2022-06-30 22:52:10 -0500

    Updating Heroku DATABASE_URL variable

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from flask import Flask
 from flask_restful import Api
@@ -12,6 +13,9 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
+uri = os.getenv('DATABASE_URL')
+if uri.startswith('postgres://'):
+    uri = uri.replace('postgres://', 'postgresql://', 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
