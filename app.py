be576362408c72c688cd115c35e3becd3364adde
Hunter Sandness <huntersandness@Hunters-MacBook-Air-3.local>
2022-06-30 22:54:56 -0500

    Updating DB var

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
-uri = os.getenv('DATABASE_URL')
-if uri.startswith('postgres://'):
-    uri = uri.replace('postgres://', 'postgresql://', 1)
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
