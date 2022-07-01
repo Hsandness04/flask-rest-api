7b575fc7bba9e846fbdecccc11ee264c2d32eac7
Hunter Sandness <huntersandness@Hunters-MacBook-Air-3.local>
2022-06-30 23:05:32 -0500

    Changed URL if statement

--- app.py
+++ app.py
@@ -13,10 +13,14 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
+
+# Set URL Heroku uses.
+uri = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', uri)
+ 
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'hunter'
